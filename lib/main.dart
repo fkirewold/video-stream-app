13898940a1b1c51373db8f1cc7325cc70d553017
Fkirewold Zerihun <134012414+fkirewold@users.noreply.github.com>
2025-09-14 21:30:29 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -47,8 +45,6 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
        ).
           mainAxisAlignment: MainAxisAlignment.center,
