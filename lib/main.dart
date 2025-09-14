4d2d1b961a2843d026c7de847096a30b693a3888
Fkirewold Zerihun <134012414+fkirewold@users.noreply.github.com>
2025-09-14 21:29:55 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -51,10 +51,6 @@ class _MyHomePageState extends State<MyHomePage> {
         // in the middle of the parent.
         child: Column(
        ).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
