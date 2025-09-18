deea9fcc4e0d3677ce7e62845d5beacfe6c55398
unknown <addimanzerihun@gmail.com>
2025-09-18 17:20:17 +0300

    create video stream page

---
+++ lib/presentation/video_stream_page.dart
@@ -0,0 +1,16 @@
+  
+import 'package:flutter/material.dart';
+
+class VideoStreamPage extends StatefulWidget {
+  const VideoStreamPage({super.key});
+
+  @override
+  State<VideoStreamPage> createState() => _VideoStreamPageState();
+}
+
+class _VideoStreamPageState extends State<VideoStreamPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
