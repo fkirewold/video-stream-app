ab55533ff20e95b1e442eeb55128390c9282eefc
unknown <addimanzerihun@gmail.com>
2025-09-22 18:21:10 +0300

    create join room method

--- lib/core/utils/signaling.dart
+++ lib/core/utils/signaling.dart
@@ -88,6 +88,66 @@ class Signaling {
       print(e.toString());
     }
   }
+  Future<void> joinRoom(String roomId, RTCVideoRenderer remoteRenderer) async {
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+    DocumentReference roomRef = firestore.collection("rooms").doc(roomId);
+    DocumentSnapshot roomSnapshot = await roomRef.get();
+    if (roomSnapshot.exists) {
+      print('Room found');
+      Map<String, dynamic> data = roomSnapshot.data() as Map<String, dynamic>;
+      var offer = data['offer'];
+      print('Got offer: $offer');
+      Map<String, dynamic> configuration = {
+        "iceServers": [
+          {
+            "urls": ["stun:stun.l.google.com:19302"]
+          }
+        ]
+      };
+      peerConnection = await createPeerConnection(configuration);
+      registerPeerConnectionListners();
+      localStream?.getTracks().forEach((track) {
+        peerConnection?.addTrack(track, localStream!);
+      });
+      peerConnection?.onTrack=(RTCTrackEvent event) {
+        if (event.streams.isNotEmpty) {
+          onAddRemoteStream?.call(event.streams[0]);
+          remoteStream = event.streams[0];
+        }
+      };
 
+      var calleeCandidatesCollection= roomRef.collection('calleeCandidates');
+      peerConnection?.onIceCandidate =(RTCIceCandidate candidate)
+      {
+          calleeCandidatesCollection.add(candidate.toMap());
+        
+      };
+
+      RTCSessionDescription offerDescription = RTCSessionDescription(
+          offer['sdp'], offer['type']);
+      await peerConnection!.setRemoteDescription(offerDescription);
+      RTCSessionDescription answer = await peerConnection!.createAnswer();
+      print('Answer created: ${answer.sdp}');
+      await peerConnection!.setLocalDescription(answer);
+      Map<String,dynamic> roomWithAnswer={
+        'answer':answer.toMap()
+      };
+      await roomRef.update(roomWithAnswer);
+
+      // Listening for remote ICE candidates
+      roomRef.collection('callerCandidates').snapshots().listen((snapshot) {
+        snapshot.docChanges.forEach((change) {
+          if (change.type == DocumentChangeType.added) {
+            Map<String, dynamic> data =
+                change.doc.data() as Map<String, dynamic>;
+            print('Got new remote ICE candidate: $data');
+            RTCIceCandidate candidate = RTCIceCandidate(
+                data['candidate'], data['sdpMid'], data['sdpMLineIndex']);
+            peerConnection?.addCandidate(candidate);
+          }
+        });
+      });
+    }
+  }
 
 }
